c502a2aca80a3f460a9bf1e5358fc3e946738215
thanosbalo <thanosbalogiannis@gmail.com>
2022-12-11 17:50:55 +0200

    changes made

--- script.js
+++ script.js
@@ -10,30 +10,56 @@ function getComputerChoise(){
     };
     return choise;
 };
+const winmsg="Human ,you won.";
+const losemsg="You lost ,fool.The computer is smarter!";
+const tiemsg="Its a tie.We will still treat you as a fool!";
 
 let userSelection;
+let roundCounter=0;
+let humanWins=0;
+let ties=0;
+let computerWins=0;
 const screen=document.querySelector(".display-screen");
 const messageOut=document.createElement("p");
 const buttons=Array.from(document.querySelectorAll("button"));
 buttons.forEach((button) => {
     button.addEventListener("click", ()=>{
+        roundCounter=roundCounter+1;
         userSelection=button.dataset.selection;
         console.log(userSelection);
         messageOut.textContent=playRound();
         screen.appendChild(messageOut);
+        console.log(humanWins,computerWins,ties);
+        if (roundCounter===5) {
+            if (humanWins>computerWins){
+                messageOut.textContent=`The final result is human ${humanWins} , ties : ${ties} and computer ${computerWins}.Human Wins!`
+            }
+            else if (humanWins<computerWins) {
+                messageOut.textContent=`The final result is human ${humanWins} , ties : ${ties} and computer ${computerWins}.Computer Wins!`
+            } else messageOut.textContent=`The final result is human ${humanWins} , ties : ${ties} and computer ${computerWins}.Bloody Tie !!`
+            screen.appendChild(messageOut);
+            resetCounters();
+        }
     })
 });
-
-const winmsg="Human ,you won.";
-const losemsg="You lost ,fool.The computer is smarter!";
-const tiemsg="Its a tie.We will still treat you as a fool!";
 
 function playRound(){
     let computerSelection = getComputerChoise();
     console.log("Computer choose:" + computerSelection);
     if (userSelection === computerSelection) {
+        ties=ties+1;
         return tiemsg;
         } else if ((userSelection==="rock" && computerSelection==="scissor") || (userSelection==="paper" && computerSelection==="rock") || (userSelection==="scissor" && computerSelection==="paper")) {
+            humanWins=humanWins+1;
             return winmsg;
-        } else return losemsg;
+        } else { 
+            computerWins=computerWins+1;
+            return losemsg};
 };
+
+function resetCounters(){
+    roundCounter=0;
+    humanWins=0;
+    computerWins=0;
+    ties=0;
+}
